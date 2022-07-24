7e693c22141eeb0de141094a9109064e797d87da
Alexey Yankov <Yankovav.wm@gmail.com>
2022-07-24 16:57:35 +0300

    Updates

---
+++ precache-manifest.50bb75bc27afc8b41ffe801c318c08e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08fa053564d0a70b46f8a5f34bcc15b6",
+    "url": "/Homework/index.html"
+  },
+  {
+    "revision": "7a60a53793480905f830",
+    "url": "/Homework/static/css/main.e179f39b.chunk.css"
+  },
+  {
+    "revision": "56bc8fc645fa3f21e055",
+    "url": "/Homework/static/js/2.9436958f.chunk.js"
+  },
+  {
+    "revision": "248fe9facfff898840212404b4cb88d9",
+    "url": "/Homework/static/js/2.9436958f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a60a53793480905f830",
+    "url": "/Homework/static/js/main.143eeebe.chunk.js"
+  },
+  {
+    "revision": "e5323a84600407dfb348",
+    "url": "/Homework/static/js/runtime-main.4d605882.js"
+  },
+  {
+    "revision": "93395709879eec4905a24e20068be5da",
+    "url": "/Homework/static/media/SpacePhoto1.93395709.jpg"
+  }
+]);
