2d0fbc8df5f0c1f24b1939b367d22967731149af
Alexey Yankov <Yankovav.wm@gmail.com>
2022-07-24 17:43:58 +0300

    added router v6

--- src/p1-main/m1-ui/u1-app/App.tsx
+++ src/p1-main/m1-ui/u1-app/App.tsx
@@ -4,10 +4,15 @@ import HW1 from '../../../p2-homeworks/h1/HW1';
 import HW2 from '../../../p2-homeworks/h2/HW2';
 import HW3 from "../../../p2-homeworks/h3/HW3";
 import HW4 from "../../../p2-homeworks/h4/HW4";
+import HW5 from "../../../p2-homeworks/h5/HW5";
+import {NavLink, Route, Routes} from "react-router-dom";
 
-function App() {
+function App () {
     return (
         <div className={s.App}>
+            <NavLink to={'/'}>main</NavLink>---
+            <NavLink to={'/login'}>login</NavLink>---
+            <NavLink to={'/profile'}>profile</NavLink>---
             <img src="SpacePhoto.jpg" alt={""}/>
             <div>react homeworks:</div>
             <HW1/>
@@ -15,6 +20,11 @@ function App() {
             <HW3/>
             <HW4/>
             <HW5/>
+            <Routes>
+                <Route path={'/'} element={<div>main</div>}/>
+                <Route path={'/login'} element={<div>login</div>}/>
+                <Route path={'/profile'} element={<div>profile</div>}/>
+            </Routes>
         </div>
     )
 }
--- src/p2-homeworks/h5/HW5.tsx
+++ src/p2-homeworks/h5/HW5.tsx
@@ -1,18 +1,13 @@
 import React from 'react'
 import Header from './Header'
-import Routes from './Routes'
+import Router from './Routes'
 
 function HW5() {
     return (
         <div>
-            {/*в gh-pages лучше работает HashRouter*/}
-            {/*<HashRouter>*/}
-
             <Header/>
 
-            <Routes/>
-
-            {/*</HashRouter>*/}
+            <Router/>
         </div>
     )
 }
