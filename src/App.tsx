b4f5e3cdadb6e55543db90b0e253e318025f660c
Alexey Yankov <yankovav.wm@gmail.com>
2022-11-14 16:02:13 +0300

    fixed header

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import ProfileHOK from './components/Profile/ProfileHOK';
+import Header from './components/UniversalComponent/Header/Header';
+
+function App() {
+  return (
+    <main className="App">
+        <Header/>
+        <ProfileHOK/>
+    </main>
+  );
+}
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './p1-main/m1-ui/u1-app/App'
-import * as serviceWorker from './serviceWorker'
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
-import store from "./p2-homeworks/h10/bll/store";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
 
-ReactDOM.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter></Provider>,
-    document.getElementById("root")
-)
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+root.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>
+);
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
