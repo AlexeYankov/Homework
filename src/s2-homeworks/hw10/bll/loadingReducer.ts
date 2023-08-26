b54933a6c4b2ff61843633dafbb21237a1598047
Alexey Yankov <yankovav.wm@gmail.com>
2023-08-26 13:48:58 +0300

    fixed strict mode

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,28 +1,30 @@
 export type InitStateType = {
-    isLoading: boolean
-}
+    isLoading: boolean;
+};
 
-const initState:InitStateType = {
+const initState: InitStateType = {
     isLoading: false,
-}
+};
 
-export const loadingReducer = (state = initState, action: LoadingActionType): InitStateType => { // fix any
+export const loadingReducer = (state: InitStateType = initState, action: LoadingActionType): InitStateType => {
+    // fix any
     switch (action.type) {
         // пишет студент  // need to fix
-        case "CHANGE_LOADING":{
-            return {...state, isLoading:action.isLoading}
+        case "CHANGE_LOADING": {
+            return {...state, isLoading: action.isLoading};
         }
         default:
-            return state
+            return state;
     }
-}
+};
 
 type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+    type: "CHANGE_LOADING";
+    isLoading: boolean;
+};
 
-export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
-})
+export const loadingAC = (isLoading: boolean) =>
+    ({
+        type: "CHANGE_LOADING",
+        isLoading,
+    } as const);
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,19 +1,19 @@
-
 const initState = {
     themeId: 1,
-}
+};
 
-export const themeReducer = (state = initState, action: ChangeThemeIdType): { themeId: number} => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeIdType): {themeId: number} => {
+    // fix any
     switch (action.type) {
         case "SET_THEME_ID":
-            return {...state, themeId:action.id}
+            console.log({themeId: action.id});
+            const newObj = Object.assign({},state)
+            return {...newObj, themeId: action.id};
 
         default:
-            return state
+            return state;
     }
-}
+};
 
-
-
-type ChangeThemeIdType = ReturnType<typeof changeThemeId>
-export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id } as const) // fix any
+type ChangeThemeIdType = ReturnType<typeof changeThemeId>;
+export const changeThemeId = (id: number) => ({type: "SET_THEME_ID", id} as const); // fix any
